241d16c9162bbfae094a4d637a1b20114ec1316f
Irvin Zhan <izhan@princeton.edu>
2014-03-29 19:59:33 -0400

    first commit for new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lektur::Application.config.secret_key_base = 'da1ae560b873371245be66fdc59cdf997102266b9b69533a01434914cbe2d03de39c5b78702b39765c5f66e913862879cbe120f234036c2edb26b441fb093e1f'
